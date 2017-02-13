79f5f95ba719192f2154778e5ecdaf51c20ebd41
Stefano Terna <stefano.terna@tomorrowdata.io>
2017-02-13 13:45:37 +0100

    bridges now receive the list of nodes to which connect, via env var NODELIST

--- bridge/docker-entrypoint.sh
+++ bridge/docker-entrypoint.sh
@@ -1,38 +1,46 @@
-bridgeconf="bridge.conf"
-
+#!/bin/bash
 
 set -e
 
-if [ "$1" = 'mosquitto' ]; then
-	chmod -R o+r .
+if [ -n "$BRIDGECONF" ] && [ -n "$NODELIST" ]; then
 
-	rm $bridgeconf
+	if [ "$1" = '/usr/sbin/mosquitto' ]; then
 
-	IFS=', ' read -r -a array <<< "$NODELIST"
+			if [ -e "$BRIDGECONF" ]; then
+				rm $BRIDGECONF
+			fi
 
-	nodeindex=1
+			IFS=', ' read -r -a array <<< "$NODELIST"
 
-	for element in "${array[@]}"
-	do
-	    echo "$element"
+			nodeindex=1
 
-		cat >> $bridgeconf <<- EOM 
+			for element in "${array[@]}"
+			do
+			    echo "Generating config to bridge with node: $element"
 
-		connection mosq-node-0$nodeindex
-		address $element:1883
-		topic /iottly/+/+/commands out
-		try_private true
-		remote_username $BRIDGE_REMOTE_USERNAME
-		remote_password $BRIDGE_REMOTE_PASSWORD
-		EOM
+				cat >> $BRIDGECONF <<- EOM 
 
-		nodeindex=$((nodeindex+1))
+				connection mosq-node-0$nodeindex
+				address $element
+				topic /iottly/+/+/commands out
+				try_private true
+				remote_username $BRIDGE_REMOTE_USERNAME
+				remote_password $BRIDGE_REMOTE_PASSWORD
+				EOM
 
-	done
+				nodeindex=$((nodeindex+1))
 
+			done
+
+			exec "$@"
+	fi
 
 	exec "$@"
-fi
 
-exec "$@"
+else
+	echo "Cannot start bridge without bridge configuration file path and node list."
+	echo "Please ensure env vars BRIDGECONF and NODELIST are defined and not empty."
+
+	exit 1
+fi
 
