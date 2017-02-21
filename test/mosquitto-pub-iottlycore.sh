cedf0b19bdf36c837ae3c9d6dc8ddb100c494cdc
Stefano Terna <stefano.terna@tomorrowdata.io>
2017-02-21 10:57:21 +0100

    fixed general HA auth parameters for mosquitto broker and bridge. tested HA configuration with 2 brokers and 2 bridge.

--- test/mosquitto-pub-iottlycore.sh
+++ test/mosquitto-pub-iottlycore.sh
@@ -5,10 +5,9 @@ MESSAGE=$2
 #docker build -t mosquittoclients -f Dockerfile-clients .
 
 docker run -it --net iottlydocker_default mosquittoclients \
- 	   mosquitto_pub -h iottlydocker_mqttbrokerbridge_1 \
+ 	   mosquitto_pub -h mqttbrokerbridge \
  	   -q 1 \
  	   -t $TOPIC \
  	   -m $MESSAGE \
- 	   -u iottlycore -P pippo
 
 #-r
