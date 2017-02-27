f54a108a739c0313af827b5b5c10ee1f290bafc4
Stefano Terna <stefano.terna@tomorrowdata.io>
2017-02-28 00:07:37 +0100

    changed topic key from int to oid

--- test/test-users.js
+++ test/test-users.js
@@ -1,12 +1,9 @@
-db.topics.insert({_id:1, topics:["/iottly/project1/device01/commands", "/iottly/project1/device01/events"]})
-
-db.topics.insert({_id:0, topics:["/iottly/+/+/commands"]})
-
-
+res = db.boardsMqttTopics.insertOne({topics:["/iottly/project1/device01/commands", "/iottly/project1/device01/events"]})
 //pwd: devicepassword
-db.users.insert({username:"device01", password:"PBKDF2$sha256$901$vsmD1hzGz6Eq40FP$yn7B8BQVgadfNyWxzvJBgS860523nMr8", topics:1, superuser:0})
+db.boardsMqttAuth.insert({username:"device01", password:"PBKDF2$sha256$901$vsmD1hzGz6Eq40FP$yn7B8BQVgadfNyWxzvJBgS860523nMr8", topics:res.insertedId, superuser:0})
 
 
+res =  db.boardsMqttTopics.insertOne({topics:["/iottly/+/+/commands"]})
 //pwd: bridgepassword
-db.users.update({username:"mosq-bridge"},{username:"mosq-bridge", password:"PBKDF2$sha256$901$G3BatbMS88ks2gpx$h2V+ufI7/UM6XjeJMHBSdQGPkdwjdWgh", topics:0, superuser:0},{ upsert: true })
+db.boardsMqttAuth.update({username:"mosq-bridge"},{username:"mosq-bridge", password:"PBKDF2$sha256$901$G3BatbMS88ks2gpx$h2V+ufI7/UM6XjeJMHBSdQGPkdwjdWgh", topics:res.insertedId, superuser:0},{ upsert: true })
 
