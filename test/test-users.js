00d7ac4b880aae49c6ca72ef933fb3b3ae7a1663
Stefano Terna <stefano.terna@tomorrowdata.io>
2017-03-09 01:08:37 +0100

    fixed posthttp ACL check; closed posthttp mosquitto new feature

--- test/test-users.js
+++ test/test-users.js
@@ -1,9 +1,12 @@
-res = db.boardsMqttTopics.insertOne({topics:["/iottly/project1/device01/commands", "/iottly/project1/device01/events"]})
+res = db.boardsMqttTopics.insertOne({topics:["/iottly/management/project1/device01/commands", "/iottly/management/project1/device01/events"]})
 //pwd: devicepassword
 db.boardsMqttAuth.insert({username:"device01", password:"PBKDF2$sha256$901$vsmD1hzGz6Eq40FP$yn7B8BQVgadfNyWxzvJBgS860523nMr8", topics:res.insertedId, superuser:0})
 
 
-res =  db.boardsMqttTopics.insertOne({topics:["/iottly/+/+/commands"]})
+res =  db.boardsMqttTopics.insertOne({topics:["/iottly/management/+/+/commands"]})
 //pwd: bridgepassword
 db.boardsMqttAuth.update({username:"mosq-bridge"},{username:"mosq-bridge", password:"PBKDF2$sha256$901$G3BatbMS88ks2gpx$h2V+ufI7/UM6XjeJMHBSdQGPkdwjdWgh", topics:res.insertedId, superuser:0},{ upsert: true })
 
+res =  db.boardsMqttTopics.insertOne({topics:["/iottly/management/+/+/events"]})
+//pwd: bridgepassword
+db.boardsMqttAuth.update({username:"posthttp"},{username:"posthttp", password:"", topics:res.insertedId, superuser:0},{ upsert: true })
